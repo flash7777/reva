c6593c15437b552fa629c220cf2ea9785cd7a1dc
flash <flash7777>
2026-06-11 19:47:56 +0200

    feat: immutable attribute + container-specific permissions
    Add two features to the decomposedfs storage driver:
    
    1. Container-specific permissions (cs3org/cs3apis#272):
       - DeleteContainer/MoveContainer checks in Delete/Move handlers
       - ACL encoding: +dc/!dc, +mc/!mc (with substring collision fix)
       - Roles updated: Editor/Manager/Coowner get DeleteContainer/MoveContainer
       - SetImmutableFile/SetImmutableContainer permissions on Manager/Coowner
    
    2. Immutable attribute (freeze/protect):
       - xattr: user.oc.immutable
       - File (freeze): content fixed, irreversible
       - Container (protect): structure fixed, reversible by managers
       - Self vs parent rule: ImmutableState = Frozen/Protected/None
       - Node methods: FreezeFile, ProtectContainer, UnprotectContainer
       - Storage interface: SetImmutable/UnsetImmutable with permission checks
       - GRPC handlers: storageprovider + gateway pass-through
       - Handler checks: Delete, Move, CreateDir, Upload
       - Stat: ResourceInfo.Immutable + Opaque immutable-state
       - WebDAV: oc:immutable property + D/NV strip from oc:permissions
       - OwnerPermissions/AddPermissions updated
    
    Tests: 21 new (node, handler, grants ACL), all pass.
    2 pre-existing failures (UpdateGrant/DenyGrant ACL round-trip).

--- pkg/conversions/role.go
+++ pkg/conversions/role.go
@@ -257,6 +257,7 @@ func NewEditorRole() *Role {
 		cS3ResourcePermissions: &provider.ResourcePermissions{
 			CreateContainer:      true,
 			Delete:               true,
+			DeleteContainer:      true,
 			GetPath:              true,
 			GetQuota:             true,
 			InitiateFileDownload: true,
@@ -264,6 +265,7 @@ func NewEditorRole() *Role {
 			ListContainer:        true,
 			ListRecycle:          true,
 			Move:                 true,
+			MoveContainer:        true,
 			RestoreRecycleItem:   true,
 			Stat:                 true,
 		},
@@ -285,6 +287,7 @@ func NewSpaceEditorRole() *Role {
 		cS3ResourcePermissions: &provider.ResourcePermissions{
 			CreateContainer:      true,
 			Delete:               true,
+			DeleteContainer:      true,
 			GetPath:              true,
 			GetQuota:             true,
 			InitiateFileDownload: true,
@@ -294,6 +297,7 @@ func NewSpaceEditorRole() *Role {
 			ListGrants:           true,
 			ListRecycle:          true,
 			Move:                 true,
+			MoveContainer:        true,
 			RestoreFileVersion:   true,
 			RestoreRecycleItem:   true,
 			Stat:                 true,
@@ -309,6 +313,7 @@ func NewSpaceEditorWithoutVersionsRole() *Role {
 		cS3ResourcePermissions: &provider.ResourcePermissions{
 			CreateContainer:      true,
 			Delete:               true,
+			DeleteContainer:      true,
 			GetPath:              true,
 			GetQuota:             true,
 			InitiateFileDownload: true,
@@ -317,6 +322,7 @@ func NewSpaceEditorWithoutVersionsRole() *Role {
 			ListGrants:           true,
 			ListRecycle:          true,
 			Move:                 true,
+			MoveContainer:        true,
 			RestoreRecycleItem:   true,
 			Stat:                 true,
 		},
@@ -355,24 +361,28 @@ func NewCoownerRole() *Role {
 	return &Role{
 		Name: RoleCoowner,
 		cS3ResourcePermissions: &provider.ResourcePermissions{
-			GetPath:              true,
-			GetQuota:             true,
-			InitiateFileDownload: true,
-			ListGrants:           true,
-			ListContainer:        true,
-			ListFileVersions:     true,
-			ListRecycle:          true,
-			Stat:                 true,
-			InitiateFileUpload:   true,
-			RestoreFileVersion:   true,
-			RestoreRecycleItem:   true,
-			CreateContainer:      true,
-			Delete:               true,
-			Move:                 true,
-			PurgeRecycle:         true,
-			AddGrant:             true,
-			UpdateGrant:          true,
-			RemoveGrant:          true,
+			GetPath:               true,
+			GetQuota:              true,
+			InitiateFileDownload:  true,
+			ListGrants:            true,
+			ListContainer:         true,
+			ListFileVersions:      true,
+			ListRecycle:           true,
+			Stat:                  true,
+			InitiateFileUpload:    true,
+			RestoreFileVersion:    true,
+			RestoreRecycleItem:    true,
+			CreateContainer:       true,
+			Delete:                true,
+			DeleteContainer:       true,
+			Move:                  true,
+			MoveContainer:         true,
+			PurgeRecycle:          true,
+			AddGrant:              true,
+			UpdateGrant:           true,
+			RemoveGrant:           true,
+			SetImmutableFile:      true,
+			SetImmutableContainer: true,
 		},
 		ocsPermissions: PermissionAll,
 	}
@@ -435,15 +445,19 @@ func NewManagerRole() *Role {
 			RestoreFileVersion:   true,
 			RestoreRecycleItem:   true,
 			Move:                 true,
+			MoveContainer:        true,
 			CreateContainer:      true,
 			Delete:               true,
+			DeleteContainer:      true,
 			PurgeRecycle:         true,
 
 			// these permissions only make sense to enforce them in the root of the storage space.
-			AddGrant:    true, // managers can add users to the space
-			RemoveGrant: true, // managers can remove users from the space
-			UpdateGrant: true,
-			DenyGrant:   true, // managers can deny access to sub folders
+			AddGrant:              true, // managers can add users to the space
+			RemoveGrant:           true, // managers can remove users from the space
+			UpdateGrant:           true,
+			DenyGrant:             true, // managers can deny access to sub folders
+			SetImmutableFile:      true, // managers can freeze files
+			SetImmutableContainer: true, // managers can protect/unprotect containers
 		},
 		ocsPermissions: PermissionAll,
 	}
